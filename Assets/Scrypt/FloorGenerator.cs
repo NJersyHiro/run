d5633bc5d0becf3572091cb2b584a387ad4b3e24
Hiromichi Yamamoto <th1khse@gmail.com>
2018-08-22 19:00:18 +0900

    run テスト用プロジェクト最初のコミット
    ランダムに地面を配置し、ボールがジャンプ移動するプロジェクトを作成

---
+++ Assets/Scrypt/FloorGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorGenerator : MonoBehaviour {
+
+	public GameObject floorPrefab;
+	//スタート地点
+	private int startPos = 0;
+	//ゴール地点
+	private int goalPos = 120;
+	//アイテムを出すx方向の範囲
+	private float posRange = 3.4f;
+
+	// Use this for initialization
+	void Start () {
+		//一定の距離ごとにアイテムを生成
+		for (int i = startPos; i < goalPos; i+=5) {
+			for (float j = -1; j <= 1; j += 1.0f) {
+					GameObject floor = Instantiate (floorPrefab) as GameObject;
+					floor.transform.position = new Vector3 (4 * j, floor.transform.position.y, i);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scrypt/SphereController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float movespeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.collider.tag == "FloorTag"){
+			this.rb.AddForce(0, 400, 0);
+
+		}
+
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		this.rb.AddForce (movement * movespeed);
+	}
+}
